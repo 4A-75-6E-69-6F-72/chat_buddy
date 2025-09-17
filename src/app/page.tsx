c367fc84305f1c69da394532e76a2611b3d97c0a
ngouoghejunior <junior_ngouoghe@pkf-researchcenter.com>
2025-09-17 23:51:35 +0200

    commented home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
 import ChatSection from "@/sections/ChatSection";
 import Footer from "@/sections/Footer";
 import Header from "@/sections/Header";
+/**
+ * ChatPage Component - Main landing page for the application
+ * 
+ * @component
+ * @example
+ * return <Home />
+ * 
+ * @description
+ * This page serves as the main entry point for users. It includes:
+ * - A header
+ * - A Messages section
+ * - A footer
+ * 
+ * @returns {JSX.Element} The rendered homepage component
+ */
+
 
 export default function Home() {
   return (
